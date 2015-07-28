14c531c962f025eed9d288ba8e0a2406148d9b1d
Sam Saint-Pettersen <s.stpettersen+github@gmail.com>
2015-07-28 11:41:08 +0100

    Update test.js

--- test.js
+++ test.js
@@ -24,7 +24,7 @@ describe('Test gulp-codo plug-in', function() {
 
     stream.write(new gutil.File({
       path: 'helloworld.coffee',
-      contents: new Buffer('#\n# Hello World\n#\nhelloworld ->\nconsole.log("Hello World!")\n')
+      contents: new Buffer('#\n# Hello World\n#\nhelloworld ->\n//TODO\n')
     }));
 
     stream.end();
